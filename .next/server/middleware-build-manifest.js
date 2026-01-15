9b5447380766239df37f46972d5fcffdb511b411
Destiny Johnson <baronlonewolf999@gmail.com>
2026-01-15 06:29:20 +0100

    commented out basepath and assetprefix to fix deployment isssues

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,18 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-b15b098e1a0bd8ba.js","static/chunks/fd9d1056-34ac56cb32b70ed4.js","static/chunks/117-d5e8e0927b46803b.js","static/chunks/main-app-86b8039777cd6eee.js"],pages:{"/_app":["static/chunks/webpack-b15b098e1a0bd8ba.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-16f2b2bafb2fbaa6.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-b15b098e1a0bd8ba.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-16f2b2bafb2fbaa6.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  basePath: isProd ? "/Crypgo" : "",
-  assetPrefix: isProd ? "/Crypgo/" : "",
-  output: "export",
+  // basePath: isProd ? "/Crypgo" : "",
+  // assetPrefix: isProd ? "/Crypgo/" : "",
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
-
- 
